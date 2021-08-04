e5be75bef927c3d5c6f269ab930374f4a629deec
kim040907 <30277526+kim040907@users.noreply.github.com>
2021-08-04 14:02:01 +0900

    Create violet.java

---
+++ violet.java
@@ -0,0 +1,7 @@
+public class Violet {
+  public ResultDto goback(MemberDto HM) {
+    ResultDto resultDto = new ResultDto();
+    resultDto.setResult("N");
+    return resultDto.getResult();
+  }
+}
